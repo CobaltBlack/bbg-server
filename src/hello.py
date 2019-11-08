a798cacfb7db6e6cd2b43d22f3bf7dd49e34fef7
CobaltBlack <eric@COBALT.localdomain>
2019-11-07 22:58:33 -0800

    moved files around

--- src/hello.py
+++ src/hello.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -10,12 +11,15 @@ def hello_world():
     app.logger.info('info visited')
     return 'Hello, World!'
     
-    
+
 @app.route('/wakeup', methods=['POST'])
 def wake_up():
     app.logger.info('wakeup called')
     # Call script to perform wakeup
-    os.system('~/wakeup &>> /var/log/wake.log')
+    PC_MAC_ADDR = '1C:1B:0D:61:ED:B5'
+    CMD_WAKEUP = 'sudo etherwake -i eth0 {} &>> /var/log/wake.log'.format(PC_MAC_ADDR)
+    app.logger.info('Executing wakeup: {}'.format(CMD_WAKEUP))
+    os.system(CMD_WAKEUP)
     return 'OK'
 
 
